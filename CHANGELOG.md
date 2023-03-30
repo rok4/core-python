569b785e7344ba56bb4b1a98fef7d3d163f47b82
CorentinPeutin <corentin.peutin@ensg.eu>
2023-03-30 14:52:33 +0200

    Merge branch 'develop' of https://github.com/rok4/core-python into feature/stockage_HTTP

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,15 +1,31 @@
 ## Summary
 
 Ajout du type de stockage HTTP.
+Lecture par système de fichier virtuel avec GDAL
 
 ## Changelog
 
 ### [Added]
 
-* Storage :
+* Storage
+    * Fonction `get_osgeo_path` permettant de configurer le bon sytème de fichier virtuel en fonction du chemin fourni, et retourne celui à utiliser dans le Open de gdal ou ogr
+
+### [Changed]
+
+* Storage
+    * la récupération d'un client S3 (`__get_s3_client`) permet de récupérer le client, l'hôte, les clés d'accès et secrète, ainsi que le nom du bucket sans l'éventuel hôte du cluster
     * Ajout de la copie de HTTP vers FILE/S3/CEPH
     * Ajout de la fonction de lecture d'un fichier HTTP, de l'existence d'un fichier HTTP et du calcul de taille d'un fichier HTTP
 
+### [Fixed]
+
+* Storage
+    * Lecture binaire S3 : mauvaise configuration du nom du bucket et de l'objet et mauvaise lecture partielle
+
+### [Removed]
+
+* Exceptions
+    * `NotImplementedError` est une exceptions native
 <!--
 ### [Added]
 
--- tests/test_Storage.py
+++ tests/test_Storage.py
@@ -138,7 +138,7 @@ def test_http_read_error(mock_http):
         mock_http.return_value = requests_instance
         data = get_data_str("http:///path/to/file.ext")
 
-    mock_http.assert_called_with("http:///path/to/file.ext")
+    mock_http.assert_called_with("http:///path/to/file.ext", stream=True)
 
 @mock.patch.dict(os.environ, {}, clear=True)
 @mock.patch("requests.get")
@@ -149,7 +149,7 @@ def test_http_read_ok(mock_http):
         mock_http.return_value = requests_instance
 
         data = get_data_str("http:///path/to/file.ext")
-        mock_http.assert_called_with("http:///path/to/file.ext")
+        mock_http.assert_called_with("http:///path/to/file.ext", stream=True)
         assert data == 'data'
     except Exception as exc:
         assert False, f"HTTP read raises an exception: {exc}"
@@ -725,3 +725,31 @@ def test_remove_s3_ok(mocked_s3_client):
         remove("s3://bucket/object.ext")
     except Exception as exc:
         assert False, f"S3 deletion raises an exception: {exc}"
+
+
+############ get_osgeo_path
+
+@mock.patch.dict(os.environ, {}, clear=True)
+def test_get_osgeo_path_file_ok():
+    try:
+        path = get_osgeo_path("file:///path/to/file.ext")
+        assert path == "/path/to/file.ext"
+    except Exception as exc:
+        assert False, f"FILE osgeo path raises an exception: {exc}"
+
+@mock.patch.dict(os.environ, {"ROK4_S3_URL": "https://a,https://b", "ROK4_S3_SECRETKEY": "a,b", "ROK4_S3_KEY": "a,b"}, clear=True)
+def test_get_osgeo_path_s3_ok():
+
+    disconnect_s3_clients()
+
+    try:
+        path = get_osgeo_path("s3://bucket@b/to/object.ext")
+        assert path == "/vsis3/bucket/to/object.ext"
+    except Exception as exc:
+        assert False, f"S3 osgeo path raises an exception: {exc}"
+
+
+@mock.patch.dict(os.environ, {}, clear=True)
+def test_get_osgeo_path_nok():
+    with pytest.raises(NotImplementedError):
+        get_osgeo_path("ceph://pool/data.ext")
