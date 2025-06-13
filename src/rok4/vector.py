0c255ef850b7b72d20351645d4a2bdab41269303
Francois Bacquelot <francois.bacquelot@ign.fr>
2025-06-13 10:21:16 +0200

    vector.py : mise en forme du skeleton du programme vector.py

--- src/rok4/vector.py
+++ src/rok4/vector.py
@@ -1,10 +1,12 @@
 """Provide class to read informations on vector data set from list or descriptor
-
+The aim is to have one module who allows to load important informations of a set of vector data
+Data can be files or S3 objects.
 The module contains the following class :
 
 - `VectorSets` - Data Vector Sets
 - `Vector` - Data Vector
-- `Layer` - Layer(name, attributes(columns, types of attributes), count, srs, bbox)
+- `Table` - Table(name, attributes(columns, types of attributes), count, srs, bbox)
+These classes would be necessary to make easily interactions between tools with these data.
 """
 
 # -- IMPORTS --
@@ -16,6 +18,9 @@ import tempfile
 # 3rd party
 from osgeo import ogr
 
+# type de structures de données
+from typing import List, Dict, Tuple
+
 # package
 from rok4.storage import copy, get_osgeo_path
 
@@ -26,29 +31,66 @@ ogr.UseExceptions()
 
 
 class VectorSet:
+    """ correspond à un ensemble de fichiers/objets vecteur
+    """
 
     @classmethod
     def from_list(cls, path: str) -> "VectorSet":
-        """Constructor method of a VectorSet from lists"""
-
+        """Constructor method of a VectorSet from lists
+        un fichier ou un objet contient une liste de chemins vers 
+        les fichiers vecteurs ou objects vecteur"""
+    
     @classmethod
     def from_descriptor(cls, path: str) -> "VectorSet":
-        """Constructor method of a VectorSet from the descriptor"""
+        """Constructor method of a VectorSet from the descriptor
+           un fichier ou un objet contient toutes les informations sur les fichiers vecteur ou objets vecteur"""
+
+    @property
+    def get_unique_srs_tables_list(srs: str)-> List[str]
+        """obtenir la liste des srs uniques des tables
+        """
+        return ["2154", "4326"]
 
 class Vector:
+    """un fichier/un objet vecteur
+    """
+    # attributs de classe
+    # chemin du fichier objet/vecteur
+    _path: str = ""
+    # la clé est le nom de la table et la valeur de l'instance de Table
+    _tables: Dict[Table]  = {[]}
 
     @classmethod
     def from_file(cls, path: str) -> "Vector":
         """Constructor method of a Vector from file"""
 
     @classmethod
-    def from_parameters(cls, path: str) -> "Vector":
+    def from_parameters(cls, path: str, tables:List[str]) -> "Vector":
         """Constructor method of a Vector from the descriptor"""
 
-class Layer:
+    @property
+    def get_unique_srs_tables_list(srs: str)->List[str]
+        """obtenir la liste des srs uniques des tables
+        """
+        return ["2154", "4326"]
 
-    def __init__(self, name, attributes, count, srs, bbox) -> "Layer":
-        """Constructor method of a Layer"""
+class Table:
+    """Une table vecteur
+    """
+
+    def __init__(self, name:str, attributes:Dict, count:int, srs:str, bbox:Tuple[float, float, float, float]) -> "Table":
+        """constructeur de Table contenant les informations directement fournies
+
+        Args:
+            name (str): _description_
+            attributes (Dict): _description_
+            count (int): _description_
+            srs (str): _description_
+            bbox (Tuple[float, float, float, float]): _description_
+
+        Returns:
+            Table: _description_
+        """
         self.__name = name
         self.__attributes = attributes
         self.__count = count
